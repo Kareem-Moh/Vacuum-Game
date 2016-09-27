ebdba6f11786d5bddd4e810cf772677914132acf
moha1097 <moha1097@UTORONTO.CA>
2016-09-27 16:50:11 +0000

    Exercise 1 files

---
+++ E1/E1soln/src/e1soln/E1.java
@@ -0,0 +1,20 @@
+/**
+ * Exercise 1
+ * @author Kareem Mohamed
+ */
+package e1soln;
+
+/**
+ * Package for E1
+ */
+public class E1 {
+
+	/**
+	 * Class for E1
+	 */
+	public static void main(String[] args) {
+		// Printing  the utorID
+		System.out.print("moha1097");
+	}
+
+}
